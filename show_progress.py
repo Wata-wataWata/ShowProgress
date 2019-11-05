a846e50fdca87cf9e0f508e857f7d213fc106256
watawatawata <didnot00@gmail.com>
2019-11-05 16:41:30 +0900

    行の選択機能を追加

--- show_progress.py
+++ show_progress.py
@@ -9,11 +9,12 @@ import random
 #
 # c = ShowProgress(始まりの数字, 終わりの数字, 一回の進む量(デフォルトは1))
 class ShowProgress:
-    def __init__(self, start: int, end: int, step=1):
+    def __init__(self, start: int, end: int, step=1, row=0):
         self.start = start
         self.end = end
         self.step = step
         self.progress = self.start
+        self.row = row
 
     def next(self):
         """
@@ -42,16 +43,28 @@ class ShowProgress:
     def set_end(self, num: float):
         self.end = num
 
+    def set_row(self, num: int):
+        self.row = num
+
     def seek_to(self, num: float):
         self.progress = num
 
     def show(self):
         progre = self.progress / (self.end - self.start) * 100
-        if progre >= 100:
+        if progre >= 100:   # 100以上なら100ぴったりに
             progre = 100
-            print("\r[*] %2.3f%%" % progre)
+
+        if self.row > 0:
+            string = "\033[%dA\r[*] %2.3f%%" % (self.row, progre)
+            string = string + ("\n" * self.row)
+        elif self.row == 0:
+            string = "\r[*] %2.3f%%" % progre
         else:
-            print("\r[*] %2.3f%%" % progre, end="")
+            string = "Invalid row."
+
+        print(string, end="")
+        if self.row == 0 and progre == 100:
+            print()
 
     def show_and_progress(self):
         while True:
@@ -71,14 +84,23 @@ class ShowProgress:
 if __name__ == "__main__":
     # 何らかのデータ
     test_list = [random.random() for x in range(100)]
-
+    test_list1 = [random.random() for x in range(100)]
     # 進捗管理クラスのインスタンス作成
     # 始まりと終わりの設定を間違うと100%にたどり着かないかも
-    progress = ShowProgress(1, len(test_list) + 1)
+    progress = ShowProgress(1, len(test_list) + 1, step=1, row=2)
+    progress1 = ShowProgress(1, len(test_list1) + 1, row=1)
     # コルーチン関数の作成
     coru = progress.show_and_progress()
+    coru1 = progress1.show_and_progress()
 
     print("[-] Process start.")
+    print("[-] Something info.")
+    print("[-------------------]")
+    print("1111111111111111111")
+    print("2222222222222222222")
+    print("3333333333333333333")
+    print("4444444444444444444")
+    print("5555555555555555555")
     for i in test_list:     # リストに何らかの処理をするようなイメージ
         # 何らかの処理 (printはしないで欲しい)
         time.sleep(0.05)
