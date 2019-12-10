e815a7d6d246d33553d67adc65b61c521a100676
Wata_wataWata <didnot001@gmail.com>
2019-12-10 09:43:21 +0900

    くるくるとバー機能の追加

--- show_progress.py
+++ show_progress.py
@@ -1,6 +1,9 @@
 import time
 import random
 
+# 例外たち
+class InvalidValue(Exception):
+    pass
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # 進捗を管理するクラス
@@ -9,12 +12,33 @@ import random
 #
 # c = ShowProgress(始まりの数字, 終わりの数字, 一回の進む量(デフォルトは1))
 class ShowProgress:
-    def __init__(self, start: int, end: int, step=1, row=0):
+    def __init__(self,
+                 start: int,
+                 end: int,
+                 step=1,
+                 row=0,
+                 bar_width=30,
+                 show_bar=True,
+                 show_roll=True,
+                 show_progress_time=True,
+                 show_remaining_time=True):
+
         self.start = start
         self.end = end
         self.step = step
         self.progress = self.start
         self.row = row
+        self.bar_width = bar_width
+
+        # 表示するか否か系変数
+        self.show_bar = show_bar
+        self.show_roll = show_roll
+        self.show_progress_time = show_progress_time
+        self.show_remaining_time = show_remaining_time
+
+        # 中で使うやつ
+        self.roll_step = 0
+        self.timestamp = timestamp()
 
     def next(self):
         """
@@ -46,32 +70,67 @@ class ShowProgress:
     def set_row(self, num: int):
         self.row = num
 
+    def set_bar_width(self, num: int):
+        self.bar_width = num
+
     def seek_to(self, num: float):
         self.progress = num
 
     def show(self):
+        string = self.get_string()
+        print(string, end="")
+
+    def get_string(self):
+        self.timestamp.stamp_start_time()
+
         progre = self.progress / (self.end - self.start) * 100
-        if progre >= 100:   # 100以上なら100ぴったりに
+        if progre >= 100:
             progre = 100
 
+        string_list = list()
+        
+        # カーソル移動の文字列
         if self.row > 0:
-            string = "\033[%dA\r[*] %s %2.3f%%" % (self.row, self.get_progress_bar(20, progre), progre)
-            string = string + ("\n" * self.row)
+            head = "\033[%dA\r" % self.row
         elif self.row == 0:
-            string = "\r[*] %2.3f%%" % progre
+            head = "\r"
         else:
-            string = "Invalid row."
+            raise InvalidValue()
 
-        print(string, end="")
+        # くるくる
+        if self.show_roll:
+            head = head + "[" + self.get_roll_string() + "]"
+        elif self.show_roll and progre == 100:
+            head = head + "[-]"
+        else:
+            head = head + "[*]"
+
+        string_list.append(head)
+
+        # パーセンテージ
+        foot = "%2.3f%%" % progre
+        string_list.append(foot)
+
+        # プログレスバー
+        if self.show_bar:
+            block_num = self.bar_width * progre / 100
+            string_list.append("[" + "▮" * int(block_num) + " " * (self.bar_width - int(block_num)) + "]")
+
+        # 経過時間
+        if self.show_progress_time:
+            string_list.append(self.timestamp.get_string_time(self.timestamp.get_time_elapsed()))
+
+        foot = "\n" * self.row
         if self.row == 0 and progre == 100:
-            print()
+            foot = foot + "\n"
+        string_list.append(foot)
 
-    
-    def get_progress_bar(self, width: int, progre: int):
-        block_num = width * progre / 100
-        bar = "[" + "▮" * int(block_num) + " " * (width - int(block_num)) + "]"
-        return bar
-    
+        return " ".join(string_list)
+
+    def get_roll_string(self):
+        roll = ('-', '\\', '|', '/', '-', '\\', '|', '/')
+        self.roll_step = (self.roll_step + 1) % len(roll)
+        return roll[self.roll_step]
 
     def show_and_progress(self):
         while True:
@@ -87,27 +146,50 @@ class ShowProgress:
 # end of class
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+class timestamp:
+    def __init__(self):
+        self.start = 0
+        self._doing = False
+    
+    def stamp_start_time(self):
+        if not self._doing:
+            self.start = time.time()
+            self._doing = True
+        else:
+            pass
+
+    def get_string_time(self, time: float):
+        msecond = int((time - int(time)) * 100)
+        second = int(time % 60)
+        minute = int(time / 60)
+        hour = int(time / 360)
+        return "%02d:%02d:%02d.%02d" % (hour, minute, second, msecond)
+
+    def reset(self):
+        self.start = 0
+        self._doing = False
+
+    def get_time_elapsed(self):
+        return time.time() - self.start
 
 if __name__ == "__main__":
     # 何らかのデータ
-    test_list = [random.random() for x in range(100)]
-    test_list1 = [random.random() for x in range(100)]
+    test_list = [random.random() for x in range(1000)]
+    test_list1 = [random.random() for x in range(1000)]
     # 進捗管理クラスのインスタンス作成
     # 始まりと終わりの設定を間違うと100%にたどり着かないかも
-    progress = ShowProgress(1, len(test_list) + 1, step=1, row=2)
+    progress = ShowProgress(1, len(test_list) + 1, step=1, row=0)
     progress1 = ShowProgress(1, len(test_list1) + 1, row=1)
     # コルーチン関数の作成
     coru = progress.show_and_progress()
     coru1 = progress1.show_and_progress()
 
+    time_stamp = timestamp()
+    time_stamp.stamp_start_time()
+
     print("[-] Process start.")
     print("[-] Something info.")
     print("[-------------------]")
-    print("1111111111111111111")
-    print("2222222222222222222")
-    print("3333333333333333333")
-    print("4444444444444444444")
-    print("5555555555555555555")
     for i in test_list:     # リストに何らかの処理をするようなイメージ
         # 何らかの処理 (printはしないで欲しい)
         time.sleep(0.05)
@@ -116,3 +198,5 @@ if __name__ == "__main__":
         next(coru)          # 進める間にprintすると表示がずれるのが欠点
     else:
         print("[-] Done.")
+    
+    print(time_stamp.get_string_time(time_stamp.get_time_elapsed()))
